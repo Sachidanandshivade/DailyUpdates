f0728251c22da4b8058cc7b17ed73365af9a2b12
Sachidanandshivade <sachidanandshivade948@gmail.com>
2026-05-05 18:02:45 +0530

    object creation

---
+++ src/OOps/Student.java
@@ -0,0 +1,16 @@
+package OOps;
+
+public class Student {
+    int id;
+    String name;
+    char style;
+
+    public void eat(){
+        System.out.println("Yummy," +name +  " Likes it");
+    }
+
+    public void sleep(){
+        System.out.print("ZZZZ " + style + " is sleeping");
+    }
+
+}
