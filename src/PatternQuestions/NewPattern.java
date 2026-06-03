c28e4210054290aa44c9b10b910ae2ba32051638
Sachidanandshivade <sachidanandshivade948@gmail.com>
2026-06-03 22:52:22 +0530

    pattern questions

---
+++ src/PatternQuestions/NewPattern.java
@@ -0,0 +1,18 @@
+package PatternQuestions;
+
+public class NewPattern {
+    public static void main(String[] args) {
+        for(int j = 5; j >= 1; j--) {
+            for(int i=1;i<=j;i++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+        for(int i =1;i<= 5;i++){
+            for(int j =1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
---
+++ src/PatternQuestions/hallowTriangle.java
@@ -0,0 +1,26 @@
+package PatternQuestions;
+
+import java.util.Scanner;
+
+public class hallowTriangle {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j =1; j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j =1;j<=i;j++) {
+                if (j == 1 || j == i || i == n) {
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+
+
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
