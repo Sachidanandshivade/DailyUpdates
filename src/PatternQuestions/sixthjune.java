9c8f2c17803a97732398ad9069b9bc1e74f8ff98
Sachidanandshivade <sachidanandshivade948@gmail.com>
2026-06-06 23:38:19 +0530

    Added Ai/Ml ppts

--- src/PatternQuestions/sixthjune.java
+++ src/PatternQuestions/sixthjune.java
@@ -104,6 +104,19 @@ public class sixthjune {
             System.out.println();
         }
 
+        System.out.println();
+        System.out.println();
+
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=5-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=2*i-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
 
     }
 }
