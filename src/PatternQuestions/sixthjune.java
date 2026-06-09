b68d66e36b507689c4129ec66c3c4315832423ea
Sachidanandshivade <sachidanandshivade948@gmail.com>
2026-06-09 18:54:11 +0530

    pattern

--- src/PatternQuestions/sixthjune.java
+++ src/PatternQuestions/sixthjune.java
@@ -117,6 +117,41 @@ public class sixthjune {
             System.out.println();
         }
 
+        System.out.println();
+        System.out.println();
+
+        for(int i=1;i<=6;i++){
+            for(int j=1;j<=5;j++){
+                if(i==1 || j == 1 || i==6 || j == 5){
+                    System.out.print("*");
+                }else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+        for(int k=1;k<=5;k++){
+            for(int i=1;i<=k;i++){
+                System.out.print("-");
+            }
+            for(int j=1;j<= k;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println();
+
+        for(int k=5;k>1;k--){
+            for(int i=1;i<=k;i++){
+                System.out.print("-");
+            }
+            for(int j=1;j<= k;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
     }
 }
