0600e7db13d5bf5bbb31733067ef9393c7daaea2
Sachidanandshivade <sachidanandshivade948@gmail.com>
2026-06-16 16:05:12 +0530

    oop

---
+++ src/OOps/Course.java
@@ -0,0 +1,18 @@
+package OOps;
+
+public class Course {
+    String course_name;
+    String Mentor_name;
+    int duration;
+
+    public void start(){
+        System.out.println("the courses started on March 23 ");
+    }
+
+    public void learn(){
+        System.out.println("we are going to learn java and python full stack ");
+    }
+    public void end(){
+        System.out.println("this course end when you get placed");
+    }
+}
--- src/OOps/StudentApp.java
+++ src/OOps/StudentApp.java
@@ -3,9 +3,18 @@ package OOps;
 public class StudentApp {
     public static void main(String[] args){
         Student s1 = new Student();
-        s1.name= "harsh";
+        s1.name= "sachi";
 
         s1.eat();
         s1.sleep();
+        System.out.println();
+        Course c1 = new Course();
+        c1.course_name = "Full Stack Programming";
+        c1.duration = 6;
+        c1.Mentor_name="sandesh";
+
+        c1.start();
+        c1.learn();
+        c1.end();
     }
 }
