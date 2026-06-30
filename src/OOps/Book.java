4e10bd714634237e088d9abde90bdd9cec329f8e
Sachidanandshivade <sachidanandshivade948@gmail.com>
2026-06-30 16:38:08 +0530

    encapsulation

---
+++ src/OOps/Book.java
@@ -0,0 +1,13 @@
+package OOps;
+
+public class Book {
+    private int pageNo;
+
+    public void setpageNo(int a){
+        pageNo = a;
+    }
+
+    public int getpageNo() {
+        return pageNo;
+    }
+}
---
+++ src/OOps/Encapsulation.java
@@ -0,0 +1,11 @@
+package OOps;
+
+
+
+public class Encapsulation {
+    public static void main(String[] args) {
+        Book b = new Book();
+        b.setpageNo(10);
+        System.out.println(b.getpageNo());
+    }
+}
