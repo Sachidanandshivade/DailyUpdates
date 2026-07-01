ba5c3c47f485774b60bc3b3d86544aa79440ae7e
Sachidanandshivade <sachidanandshivade948@gmail.com>
2026-07-01 12:24:41 +0530

    variables

---
+++ src/JAVA_SCRIPT/Variable.js
@@ -0,0 +1,13 @@
+let a = 10;
+let b = 5;
+
+document.writeln("Arithmetic operator");
+document.writeln("<br>");
+
+document.writeln(a + b); //15
+document.writeln("<br>");
+
+document.writeln(a - b); //5
+document.writeln("<br>");
+
+document.writeln(a * b); //50
