a55ffb8b0d1dcbd8b738fbcc4a36d108a062533b
Sachidanandshivade <sachidanandshivade948@gmail.com>
2026-07-02 12:33:47 +0530

    datatypes

---
+++ src/JAVA_SCRIPT/Operators.js
@@ -0,0 +1,22 @@
+document.writeln("<br>");
+let age = 18;
+console.log(age > 18 && age < 30); // Output: false
+console.log(age < 18 || age > 30); // Output: true
+document.writeln("check output on console");
+document.writeln("<br>");
+document.write(2 & 4); // 0
+document.write(2 | 4); // 6
+document.write("<br>");
+
+document.writeln("Ternary operators");
+document.writeln("<br>");
+// condition ? "Exp 1 (true" : "Exp 2 (false"
+let canVote = (age >= 18) ? "Yes" : "No";
+document.writeln(canVote); // Output: Yes
+// to find weather num is even or odd
+document.writeln("<br>");
+let num = 16;
+let res = (num % 2 === 0) ? "Even" : "Odd";
+document.writeln(res); // Output: Even
+
+document.writeln("<brNormally I can help with things like this, but I don't seem to have access to that content. You can try again or ask me for something else.
