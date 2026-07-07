f9496496f5da6ec6d37f759521d9e69b17733b63
Sachidanandshivade <sachidanandshivade948@gmail.com>
2026-07-07 18:37:18 +0530

    arrays

---
+++ src/JAVA_SCRIPT/Array.js
@@ -0,0 +1,13 @@
+let arr = [101,"Abhi",6.9,true]
+console.log(arr[3])
+console.log(arr[2])
+console.log(arr)
+
+arr.push(9)
+
+
+//replacing
+arr.splice(1,2,"sachi", 17)
+console.log(arr)
+arr.pop();
+console.log(arr)
