87815697d0faa94a39a4a1b5737e087df6ff21b0
Sachidanandshivade <sachidanandshivade948@gmail.com>
2026-07-08 18:42:26 +0530

    array methods

--- src/JAVA_SCRIPT/Array.js
+++ src/JAVA_SCRIPT/Array.js
@@ -4,10 +4,18 @@ console.log(arr[2])
 console.log(arr)
 
 arr.push(9)
+arr.unshift(10)
+
+
 
 
 //replacing
 arr.splice(1,2,"sachi", 17)
 console.log(arr)
 arr.pop();
+arr.shift()
+console.log(arr.slice(1,3))
 console.log(arr)
+
+let a = [3,7,2,5,8,1]
+console.log(a.sort().reverse())
