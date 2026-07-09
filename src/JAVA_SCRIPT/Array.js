6f6bc9910b6c649d327519e906d04423e7c20de0
Sachidanandshivade <sachidanandshivade948@gmail.com>
2026-07-09 23:16:40 +0530

    map,filter,alert,procedure

--- src/JAVA_SCRIPT/Array.js
+++ src/JAVA_SCRIPT/Array.js
@@ -19,3 +19,28 @@ console.log(arr)
 
 let a = [3,7,2,5,8,1]
 console.log(a.sort().reverse())
+
+let mul5 = arr.map(n => n*5)
+console.log(mul5)
+
+let even = arr.filter(n => n%2 === 0)
+console.log(even)
+
+let sum = arr.reduce((sum,n)=> sum+arr,0)
+console.log(sum)
+
+let b =[10,11,12]
+console.log(b.concat(a))
+
+//join
+let words = ["I", "am", "Knowledgeable"] // I am Knowledgeable
+let sentence = words.join(" ")
+console.log(sentence)
+
+//Destructuring array
+let mentor = ["Gama", "Sandesh", "Sharana"]
+let[Web, Python, Java] = mentor
+console.log(mentor)
+console.log(Web) //Gama
+console.log(Java) //Sharana
+console.log(Python) //Sandesh
