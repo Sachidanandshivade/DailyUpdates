5b5036cf4512cfaedeae7937484811e5d2fa6b1e
Sachidanandshivade <sachidanandshivade948@gmail.com>
2026-08-03 18:50:37 +0530

    final keyword

---
+++ src/OOps/Final.java
@@ -0,0 +1,11 @@
+final class Final {
+
+    final int a;
+    a=10;
+
+    final void display() {
+        a = 20;
+        System.out.println("Value of a: " + a);
+    }
+
+}
---
+++ src/OOps/Finall.java
@@ -0,0 +1,9 @@
+class Finall extends Final{
+    public void display() {
+        System.out.println("This is the display method of Final2 class.");
+    }
+    public static void main(String[] args) {
+    Finall f = new Finall();
+    f.display();
+    }
+}
